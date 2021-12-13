0bfc0496c699a5f5db67c31c61981900db2c9116
Jadedvi104 <jadedvi104@gmail.com>
2021-12-13 16:45:40 +0700

    Update claimPS.sol

---
+++ claimPS.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @author ECIO Engineering Team
+/// @title Claimtoken Smart Contract
