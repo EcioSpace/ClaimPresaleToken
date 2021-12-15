5c4cf31215570097d5863e756768cbe93cd70f12
Jadedvi104 <jadedvi104@gmail.com>
2021-12-15 14:09:19 +0700

    Update claimPS.sol

--- claimPS.sol
+++ claimPS.sol
@@ -4,14 +4,49 @@ pragma solidity ^0.8.3;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /// @author ECIO Engineering Team
 /// @title Claimtoken Smart Contract
 
 
-//BUSD token address.
-address public busdTokenAddress;
+contract Claimtoken is Ownable, ReentrancyGuard {
 
-function setBUSDTokenAddress(address _address) public onlyOwner{
-    busdTokenAddress = _address;
+
+  address owner;
+  address preSalesAddress
+
+  constructor(
+    address _preSalesAddress
+  ) {
+      owner = msg.sender;
+      preSalesAddress = _preSalesAddress;
+  }
+
+  //BUSD token address.
+  address public busdTokenAddress;
+
+  //ECIO token address.
+  address public ecioTokenAddress;
+
+  function setBUSDTokenAddress(address _address) public onlyOwner{
+      busdTokenAddress = _address;
+  }
+
+  function setECIOTokenAddress(address _address) public onlyOwner{
+      ecioTokenAddress = _address;
+  }
+
+   function checkPresale(address _customerAddress) public view returns (uint256) {
+        return accountBalances[_customerAddress];
+   }
+
+
+    function _tranferToken(
+      ) public payable nonReentrant {
+      IERC20(ecioTokenAddress).transferFrom(address(this), msg.sender, amount);
+    }
+
+
+
 }
